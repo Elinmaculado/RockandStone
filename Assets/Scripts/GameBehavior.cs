7eb9f9132d431efbb622a7d8d82d58a81ff66d41
Miguel239g <127420871+Elinmaculado@users.noreply.github.com>
2024-09-06 12:46:20 -0600

    Firstt Commit
    Added:
    -Add money on click
    -Planet darkens upon a certain limit

---
+++ Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBehavior : MonoBehaviour
+{
+    //Variables
+    //Clicking / Score
+    public Text scoreText;
+    public float currentScore;
+    public float hitPower;
+    public float scoreIncreasePerSecond;
+    public float x;
+
+    //Planet Color
+    //public float darkenAmount = 0.1f;
+    public float planetDarkLimit;
+    [SerializeField] private Color currentColor;
+    private Image image;
+
+    //Shop
+    public Text shop1Text;
+    public int shop1Price;
+
+    public Text shop2Text;
+    public int shop2Price;
+
+    public Text ammount1Text;
+    public int ammount1;
+    public float ammount1Profit;
+    
+    public Text ammount2Text;
+    public int ammount2;
+    public float ammount2Profit;
+
+
+    void Start()
+    {
+        image = GetComponentInParent<Image>();
+        currentColor = image.color;
+        currentScore = 0;
+        //hitPower = 1;
+        scoreIncreasePerSecond = 1;
+        x = 0f;
+    }
+
+    void Update()
+    {
+        //Click
+        scoreText.text = (int)currentScore + " $";
+        scoreIncreasePerSecond = x * Time.deltaTime;
+        currentScore += scoreIncreasePerSecond;
+        /*
+        //Shop
+        shop1Text.text = "Autoclick Tier 1: " + shop1Price + " $";
+        shop2Text.text = "Autoclick Tier 2: " + shop2Price + " $";
+
+        ammount1Text.text = "Tier 1: " + ammount1 + "arts $: " + ammount1Profit + "/s";
+        ammount2Text.text = "Tier 1: " + ammount2 + "arts $: " + ammount2Profit + "/s";
+        */
+    }
+
+    public void Hit()
+    {
+        currentScore += hitPower;
+
+        // Obtener el porcentaje de progreso del score
+        float progress = Mathf.Clamp01(currentScore / 10); // Se asegura de que el progreso esté entre 0 y 1
+
+        // Obtener el color actual de la imagen
+        currentColor = image.color;
+
+        // Calcular los nuevos valores de color basados en el progreso
+        currentColor.r = Mathf.Lerp(1f, 0f, progress); // De 1 a 0 conforme a progress
+        currentColor.g = Mathf.Lerp(1f, 0f, progress);
+        currentColor.b = Mathf.Lerp(1f, 0f, progress);
+
+        // Asignar el nuevo color a la imagen
+        image.color = currentColor;
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Assets obtained from:
+https://opengameart.org
+and
+https://kenney.nl
+
+Mentions:
+- OjiroChan: https://opengameart.org/users/ohjirochan
